6af9a51148d1315fc280168b5bce17072619cdc0
Brand <blubrandi0@gmail.com>
2020-04-29 19:01:14 -0600

    Login successful, redirects to StoryVC upon login.

---
+++ ForestAdventures/ForestAdventures/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  ForestAdventures
+//
+//  Created by Brandi Bailey on 4/29/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
